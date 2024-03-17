be2760545785fb686aed60fb43b4f9ff0bc78a24
Omar Mohamed <72778139+MrGhost23@users.noreply.github.com>
2024-03-18 01:44:15 +0200

    added shadcn and some ui components

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Ui from "./pages/Ui";
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="about" element={<div>About</div>} />
+        <Route path="ui" element={<Ui />} />
       </Routes>
     </Router>
   );
---
+++ client/src/pages/Ui.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+const Ui = () => {
+  return (
+    <div>
+      <Button variant="outline">Click me</Button>
+    </div>
+  );
+};
+export default Ui;
