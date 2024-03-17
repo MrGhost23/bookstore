4d1f534243228a9f2a13e38fcc640e8742b992d5
Omar Mohamed <72778139+MrGhost23@users.noreply.github.com>
2024-03-17 02:30:35 +0200

    connected db to the server

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { BookModule } from './book/book.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGODB_URI),
+    BookModule,
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
