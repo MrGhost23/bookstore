5d8c9c09f9db1c962c27bca02ef8d2ccdf7d2da9
Omar Mohamed <72778139+MrGhost23@users.noreply.github.com>
2024-03-31 01:26:53 +0200

    some changes, not the final result

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,60 +1,67 @@
-import { RootState } from "@/store/store";
+import { AppDispatch, RootState } from "@/store/store";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { fetchBook } from '@/store/slices/bookSlice';
-import bookimg from '../Assets/img/Picsart_24-03-29_03-07-07-179.png'
+import { fetchBook } from "@/store/slices/bookSlice";
+import bookimg from "../Assets/img/Picsart_24-03-29_03-07-07-179.png";
 import BooksCard from "@/components/booksCard/BooksCard";
 import BestSeller from "@/components/Best Seller/BestSeller";
 import SoonBoks from "@/components/soonbooks/SoonBooks";
 import BookDetails from "@/components/BookDetails/BookDetails";
+import BookDetailsTest from "@/components/BookDetails/BookDetailsTest";
 
 const Home = () => {
+  const books = useSelector((state: RootState) => state.book.books);
+  console.log(books);
 
-  const books = useSelector((state: RootState) => state.book.books)
-  console.log(books)
-
-  const dispatch = useDispatch()
+  const dispatch: AppDispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(fetchBook())
-  }, [])
+    dispatch(fetchBook());
+  }, [dispatch]);
 
-
-
-  return <>
-    <div className="container mt-20 bg-orange-100">
-      <div className="grid grid-cols-2 ">
-        <div className="flex flex-col justify-center align-middle">
-          <h2 className="mb-3">Build your library</h2>
-          <p className="mb-3 font-bold">Online shop where you can find your favorite book</p>
-          <button type="button" className="text-white w-36 bg-orange-700  hover:bg-orange-800 focus:ring-4 focus:ring-orange-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-orange-600 dark:hover:bg-orange-700 focus:outline-none dark:focus:ring-orange-800">Shop now</button>
-        </div>
-        <div className="flex flex-row justify-center">
-          <img src={bookimg} className="h-96" alt="book img" />
+  return (
+    <>
+      <div className="px-8 my-5">
+        <div className="grid grid-cols-1 relative lg:grid-cols-2 bg-sky-100 px-4 py-8 rounded-md">
+          <div className="flex flex-col justify-center align-middle">
+            <h2 className="mb-3">Build your library</h2>
+            <p className="mb-3 font-bold">
+              Online shop where you can find your favorite book
+            </p>
+            <button
+              type="button"
+              className="text-white w-36 bg-sky-700  hover:bg-sky-800 focus:ring-4 focus:ring-sky-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-sky-600 dark:hover:bg-sky-700 focus:outline-none dark:focus:ring-sky-800"
+            >
+              Shop now
+            </button>
+          </div>
+          <div className="flex flex-row absolute right-0 -bottom-10 lg:static justify-center">
+            <img src={bookimg} className="h-24 lg:h-72" alt="book img" />
+          </div>
         </div>
       </div>
-    </div>
 
-    <div className="flex justify-center align-middle my-16">
+      <div className="flex justify-center align-middle my-16">
+        {/* <CategorySlider/> */}
+      </div>
 
-      {/* <CategorySlider/> */}
-    </div>
+      <div className="my-16">
+        <BooksCard />
+      </div>
+      {/* 
+      <div className="my-16">
+        <BestSeller />
+      </div>
 
-    <div className="my-16">
-      <BooksCard />
-    </div>
+      <div className="my-16">
+        <SoonBoks />
+      </div> */}
 
-    <div className="my-16">
-      <BestSeller />
-    </div>
-
-    <div className="my-16">
-      <SoonBoks />
-    </div>
-
-    {/* <div className="my-20">
-      <BookDetails />
-    </div> */}
-  </>;
+      <div className="my-20">
+        {/* <BookDetails /> */}
+        <BookDetailsTest />
+      </div>
+    </>
+  );
 };
 export default Home;
