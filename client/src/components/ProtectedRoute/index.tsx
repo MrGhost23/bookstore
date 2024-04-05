2dfc1d602d50fd1e7abe7d24ca4bb1b3af74fa6e
Omar Mohamed <72778139+MrGhost23@users.noreply.github.com>
2024-04-05 03:17:31 +0200

    finished auth

--- client/src/components/ProtectedRoute/index.tsx
+++ client/src/components/ProtectedRoute/index.tsx
@@ -1,13 +1,56 @@
+import { useEffect, useState } from "react";
 import { Navigate } from "react-router-dom";
+import axios from "axios";
+import * as React from "react";
 
+import { ReactNode } from "react";
+import { useDispatch } from "react-redux";
+import { AppDispatch } from "@/store/store";
+import { setUser } from "@/store/slices/userSlice";
+import toast from "react-hot-toast";
 
-const ProtectedRoute = (props)=>{
-if(localStorage.getItem('userToken')!==null){
-    return props.children;
-}else{
-    return <Navigate to='/login'/>
-}
-    
-}
+const ProtectedRoute: React.FC<{ children: ReactNode }> = ({ children }) => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const dispatch: AppDispatch = useDispatch();
 
-export default ProtectedRoute
+  useEffect(() => {
+    const validateToken = async () => {
+      const token = localStorage.getItem("userToken");
+      if (token) {
+        try {
+          const response = await axios.post(
+            "http://localhost:5000/auth/validate",
+            {},
+            {
+              headers: {
+                Authorization: `Bearer ${token}`,
+              },
+            }
+          );
+          if (response.data._id) {
+            setIsAuthenticated(true);
+            dispatch(setUser(response.data));
+          }
+        } catch (error) {
+          toast.error(error.message);
+        }
+      }
+      setIsLoading(false);
+    };
+
+    validateToken();
+  }, [dispatch]);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (!isAuthenticated) {
+    return <Navigate to="/login" />;
+  }
+
+  return <>{children}</>;
+};
+
+export default ProtectedRoute;
